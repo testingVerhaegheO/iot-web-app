b6cdb8ed0c8aae6788eca80decf6ba92e206d3a8
testingVerhaegheO <olivier.verhaeghe@delaware.pro>
2019-04-05 15:28:46 +0200

    iot+webapp013

---
+++ testing.ps1
@@ -0,0 +1,17 @@
+﻿#Install-Module -Name Az -AllowClobber
+#Import-Module -Name Az
+
+#Connect-AzAccount
+Get-ChildItem -File -Recurse | Set-AzStorageBlobContent -Container "armtemplates" -Blob "armtemplatesvo"
+
+$context = New-AzureStorageContext -ConnectionString "DefaultEndpointsProtocol=https;AccountName=armstoragevo;AccountKey=KTUASgXDbNLrfXsRZyN8qb27VUBR33ESvLl6gibPBq4rBC3+Gxs+lMTgoGIDOah33L/G7lZwRC2R+D+NvaNjfw==;EndpointSuffix=core.windows.net"
+$blobs = Get-AzStorageBlob -Container "armtemplates" -Context $context
+Set-AzStorageBlobContent -Blob "armtemplatesvo" -Container "armtemplates" -File ".\main.json" -Force
+
+Get-AzureStorageContainer -Container "armtemplates" | Set-AzureStorageBlobContent -File ".\main.json" -Blob "armtemplatesvo"
+
+Get-AzStorageBlob -Container "armtemplates" -Context $context | select Name
+
+Get-AzureStorageBlob -Container "armtemplatesvo"
+
+Install-Module -Name Az -AllowClobber -Scope CurrentUser
